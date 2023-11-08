44e962016d375da77589d71b5c820f2aaee0e363
Satish Addanki <satishaddanki321@gmail.com>
2023-11-08 13:50:43 +0530

    satish

--- SelectXml/Form1.Designer.cs
+++ SelectXml/Form1.Designer.cs
@@ -31,6 +31,8 @@
             openFileDialog1 = new OpenFileDialog();
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
+            comboBox3 = new ComboBox();
             SuspendLayout();
             // 
             // openFileDialog1
@@ -59,11 +61,32 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // button3
+            // 
+            button3.Location = new Point(217, 113);
+            button3.Margin = new Padding(1);
+            button3.Name = "button3";
+            button3.Size = new Size(77, 21);
+            button3.TabIndex = 2;
+            button3.Text = "FillCombo";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(78, 103);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(121, 23);
+            comboBox3.TabIndex = 5;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(329, 165);
+            Controls.Add(comboBox3);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Margin = new Padding(1);
@@ -77,5 +100,7 @@
         private OpenFileDialog openFileDialog1;
         private Button button1;
         private Button button2;
+        private Button button3;
+        private ComboBox comboBox3;
     }
 }
